a561134d440315cec4855a78832852e3ff4cbdf0
Murat Dirgenalioğlu <muratd38@hotmail.com>
2023-02-21 17:50:25 +0300

    "Project completed"

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Footer from "./template/Footer"
+import Header from "./template/Header"
+import Navbar from "./template/Navbar"
+import PageContent from "./template/PageContent"
+
+
+function App() {
+
+  return (<>
+    <Navbar/>
+    <Header/>
+    <PageContent/>
+    <Footer/>
+  </>
+
+  )
+
+}
+
+export default App
+
+
